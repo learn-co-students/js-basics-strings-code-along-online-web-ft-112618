d73cca585414fd354aa59d400eec6d65cf15601e
o-vuong <oscarvuong@gmail.com>
2019-03-11 17:23:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Oscar';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-let shortGreeting = `Welcome, ${currentUser.charAt(0)} !`;
+let shortGreeting = `Welcome, ${currentUser.charAt(0)} !lea`;
